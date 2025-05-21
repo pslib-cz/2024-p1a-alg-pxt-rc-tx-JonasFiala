10f104e848e677275b7d10ca77dceba63bf3b69d
JonasFiala <jonas.fiala.024@pslib.cz>
2025-05-21 11:21:57 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+radio.setGroup(15);
+radio.setTransmitSerialNumber(true)
 
+basic.forever(function () {
+    let x = input.acceleration(Dimension.X);
+    let y = input.acceleration(Dimension.Y);
+    radio.sendValue("x", x);
+    radio.sendValue("y", y);
+    basic.pause(100);
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Stop");
+})
+input.onButtonPressed(Button.B, function () {
+})
